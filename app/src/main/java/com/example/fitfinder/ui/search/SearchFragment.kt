c668d3ca6b78ede998e5a0d3bbb6ba2a11eb5d79
Omri Gawi <93129092+OmriGawi@users.noreply.github.com>
2023-08-30 20:21:55 +0300

    Start working on search functionality

--- app/src/main/java/com/example/fitfinder/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/fitfinder/ui/search/SearchFragment.kt
@@ -6,21 +6,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.lifecycle.ViewModelProvider
 import com.example.fitfinder.R
 import com.example.fitfinder.data.model.SkillLevel
+import com.example.fitfinder.data.model.WorkoutTime
+import com.example.fitfinder.data.repository.sportcategories.SportCategoriesRepository
 import com.example.fitfinder.databinding.FragmentSearchBinding
+import com.example.fitfinder.viewmodel.ViewModelFactory
+import com.example.fitfinder.viewmodel.search.SearchViewModel
+import com.example.fitfinder.viewmodel.sportcategories.SportCategoriesViewModel
 
 class SearchFragment : Fragment() {
 
     // binding
     private lateinit var binding: FragmentSearchBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    // View-model
+    private lateinit var searchViewModel: SearchViewModel
+    private lateinit var sportCategoriesViewModel: SportCategoriesViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Init ViewModels
+        sportCategoriesViewModel = ViewModelProvider(this, ViewModelFactory(SportCategoriesRepository()))[SportCategoriesViewModel::class.java]
+
+        // Initialize sport categories dropdown
+        sportCategoriesViewModel.fetchSportCategories()
+        sportCategoriesViewModel.sportCategories.observe(viewLifecycleOwner) { categories ->
+            val categoriesAdapter = ArrayAdapter(
+                requireContext(),
+                R.layout.dropdown_item,
+                categories
+            )
+            binding.acSportCategory.setAdapter(categoriesAdapter)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Adapter for skill levels
+        val skillLevels = SkillLevel.values().map { it.name }
+        val skillLevelsAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, skillLevels)
+        binding.acSkillLevel.setAdapter(skillLevelsAdapter)
+
+        // Adapter for Workout Times
+        val workoutTimes = WorkoutTime.values().map { it.name }
+        binding.multiSelectionWorkoutTimes.items = workoutTimes
     }
 
 }
---
+++ app/src/main/java/com/example/fitfinder/viewmodel/search/SearchViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.fitfinder.viewmodel.search
+
+class SearchViewModel() {
+}
